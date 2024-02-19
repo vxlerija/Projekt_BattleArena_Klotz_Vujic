8e35dc2bc36f537add929c517690355e1a95ded3
vxlerija <valerija.klotz@hak-reutte.ac.at>
2024-02-19 11:39:01 +0100

    abstract baseclass with attributes and methods created

---
+++ src/battleArena/BaseCharacter.java
@@ -0,0 +1,38 @@
+package battleArena;
+public abstract class BaseCharacter {
+	
+	private String name;
+	private int livingPoints;
+	
+	
+	
+	public BaseCharacter(String name, int livingPoints) {
+		super();
+		this.name = name;
+		this.livingPoints = livingPoints;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getLivingPoints() {
+		return livingPoints;
+	}
+	public void setLivingPoints(int livingPoints) {
+		this.livingPoints = livingPoints;
+	}
+	
+	public abstract void getDamage(int Points);
+	
+	public abstract void attack(BaseCharacter enemy);
+	
+	public abstract boolean specialAbilityActive();
+	 
+	public abstract boolean specialAbilityDeactive();
+	
+	
+	
+
+}
