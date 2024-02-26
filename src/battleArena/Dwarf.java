7d0090b01d37f6c27dfcb103f5d73688a8c656f2
angelinavujic <angelina.vujic@hak-reutte.ac.at>
2024-02-26 10:02:54 +0100

    Class Dwarf created

--- src/battleArena/Dwarf.java
+++ src/battleArena/Dwarf.java
@@ -1,5 +1,37 @@
 package battleArena;
 
-public class Dwarf {
+public class Dwarf extends BaseCharacter{
+
+	public Dwarf(String name, int livingPoints) {
+		super(name, livingPoints);
+	}
+
+	@Override
+	public void getDamage(int Points) {
+	
+		
+	}
+
+	@Override
+	public void attack(BaseCharacter enemy) {
+		
+		
+	}
+
+	@Override
+	public boolean specialAbilityActive() {
+		return false;
+	}
+
+	@Override
+	public boolean specialAbilityDeactive() {
+		return false;
+	}
+	
+	
+	
+	
+	
+	
 
 }
