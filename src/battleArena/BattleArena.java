aeae7e4b704ca542e836301cb4ce4c656d68c34c
vxlerija <valerija.klotz@hak-reutte.ac.at>
2024-03-11 10:59:07 +0100

    began creating methods

--- src/battleArena/BattleArena.java
+++ src/battleArena/BattleArena.java
@@ -1,6 +1,18 @@
 package battleArena;
 
+import java.util.Scanner;
+
 public class BattleArena {
 	BaseCharacter p1 = new Dwarf ("Dwarf", 100);
 	BaseCharacter p2 = new Dragon ("Dragon", 100);
+	
+	public void menu() {
+		System.out.println(1 + " - attack" + "\n" + 2 + " - activate special attack" + "\n" + 3 + " - deactivate special attack");
+		Scanner obj = new Scanner(System.in);
+		int option = obj.nextInt();
+		switch(option) {
+		case 0: 
+			
+		}
+	}
 }
